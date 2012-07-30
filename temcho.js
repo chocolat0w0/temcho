335b07b2a40d5e2644994616d116212fe293ea17
chocoffice <shiori.yamazaki.1002@gmail.com>
2012-07-30 14:46:45 +0900

    【追加】タイマー０で停止

--- temcho.js
+++ temcho.js
@@ -7,47 +7,59 @@ temcho.Timer = function() {
 };
 
 temcho.Timer.prototype.getStatus = function() {
-	return status;
+	return this.status;
 };
 
 temcho.Timer.prototype.setStatus = function(nextStatus) {
-	status = nextStatus;
+	this.status = nextStatus;
 };
 
 temcho.Timer.prototype.getTime = function() {
-	return time;
+	return this.time;
+};
+
+temcho.Timer.prototype.setTime = function(nextTime) {
+	this.time = nextTime;
 };
 
 temcho.Timer.prototype.startUp = function() {
 	this.setStatus("stop");
-	time = 25.0;
+	this.setTime(5.0);
 	btn_label = "start";
 	
 	if ((idTimer = document.getElementById('timer')) != null) {
-		idTimer.innerHTML = time;
+		idTimer.innerHTML = this.getTime();
 	}
 	
 };
 
 temcho.Timer.prototype.clickBtn = function() {
+	var initialTime;
+	var restTime;
+	var maxTime = this.getTime();
 	if (this.getStatus() === "stop") {
 		initialTime = new Date().getTime();
 		this.setStatus("work");
 		btn_label = "interrupt";
-		setInterval(function() {
-			time = 25 - ((new Date().getTime() - initialTime)/1000);
-			if ((timer = document.getElementById('timer')) != null) {
-				timer.innerHTML = time.toFixed(1);
+	
+		Timer1 = setInterval(function() {
+			restTime = maxTime - ((new Date().getTime() - initialTime)/1000);
+		
+			if ((idTimer = document.getElementById('timer')) != null) {
+				idTimer.innerHTML = restTime.toFixed(1);
 			}
-		}, 100);		
+			if (restTime <= 0) {
+				clearInterval(Timer1);
+				alert("とまったよ！");
+			}		
+		}, 100);
 	}
 	
 	
 	if ((idBtn = document.getElementById('btn')) != null) {
 		idBtn.value = btn_label;
 	}
-		
-	// debug用
+		// debug用
 	if ((idStatus = document.getElementById('status')) != null) {
 		idStatus.innerHTML = "work";
 	}
