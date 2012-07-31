fcf5430062a7ee3b0818869f8a209f22694495df
chocoffice <shiori.yamazaki.1002@gmail.com>
2012-07-31 16:16:36 +0900

    設定時間が0になった場合、モードが自動的に切り替わるテストを追加。実装コードはまだ。

--- temcho.js
+++ temcho.js
@@ -9,23 +9,32 @@ temcho.Timer = function() {
 temcho.Timer.prototype.getStatus = function() {
 	return this.status;
 };
-
 temcho.Timer.prototype.setStatus = function(nextStatus) {
 	this.status = nextStatus;
 };
-
 temcho.Timer.prototype.getTime = function() {
 	return this.time;
 };
-
 temcho.Timer.prototype.setTime = function(nextTime) {
 	this.time = nextTime;
+};
+temcho.Timer.prototype.getBtnLabel = function() {
+	return this.btn_label;
+};
+temcho.Timer.prototype.setBtnLabel = function(nextLabel) {
+	this.btn_label = nextLabel;
+};
+temcho.Timer.prototype.getMode = function() {
+	return this.mode;
+};
+temcho.Timer.prototype.setMode = function(nextMode) {
+	this.mode = nextMode;
 };
 
 temcho.Timer.prototype.startUp = function() {
 	this.setStatus("stop");
 	this.setTime(5.0);
-	btn_label = "start";
+	this.setBtnLabel("start");
 	
 	if ((idTimer = document.getElementById('timer')) != null) {
 		idTimer.innerHTML = this.getTime();
@@ -40,7 +49,7 @@ temcho.Timer.prototype.clickBtn = function() {
 	if (this.getStatus() === "stop") {
 		initialTime = new Date().getTime();
 		this.setStatus("work");
-		btn_label = "interrupt";
+		this.setBtnLabel("interrupt");
 	
 		Timer1 = setInterval(function() {
 			restTime = maxTime - ((new Date().getTime() - initialTime)/1000);
@@ -57,11 +66,12 @@ temcho.Timer.prototype.clickBtn = function() {
 	
 	
 	if ((idBtn = document.getElementById('btn')) != null) {
-		idBtn.value = btn_label;
+		idBtn.value = this.getBtnLabel();
 	}
-		// debug用
+	
+	// debug用
 	if ((idStatus = document.getElementById('status')) != null) {
-		idStatus.innerHTML = "work";
+		idStatus.innerHTML = this.getStatus();
 	}
 	// debug用ここまで
 	
