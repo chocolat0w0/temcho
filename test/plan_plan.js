30183efb0502754912251eacc302f1b4b6e383fa
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2012-08-02 23:56:49 +0900

    [追加]タスク予定の記入ページを追加

---
+++ test/plan_plan.js
@@ -0,0 +1,13 @@
+AsyncTestCase("temchoPlanTest", {
+	setUp: function () {
+		// htmlのfixtureを作成する
+		// http://code.google.com/p/js-test-driver/wiki/HtmlDoc
+	},
+	
+	tearDown: function () {
+	},
+	
+	"test xxx should xxx": function() {
+		
+	}
+});
