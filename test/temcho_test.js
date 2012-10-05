3d2846e5257b12a689ac1f2fdb99adbd71939fc8
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2012-10-05 23:37:21 +0900

    $B!Z%j%U%!%/%?%j%s%0![%/%m!<%8%c(B

--- test/temcho_test.js
+++ test/temcho_test.js
@@ -1,93 +1,91 @@
 AsyncTestCase("temchoTest", {
 	setUp: function () {
-		this.timer = new temcho.Timer();
-		var myTimer = tttimer;
 	},
 	
 	tearDown: function () {
 	},
 	
 	"test startUp should show the initial mode (mode:off/status:stop)": function() {
-		this.timer.startUp();
+		startUp();
 		
-		assertEquals("off", this.timer.getMode());
-		assertEquals("stop", myTimer.getStatus());
-		assertNumber(this.timer.getTime());
-		assertEquals(TIME_ON, this.timer.getTime());
-		assertEquals("start", this.timer.getBtnLabel());
+		assertEquals("off", timerMode.get());
+		assertEquals("stop", timerStatus.get());
+		assertNumber(timerMax.get());
+		assertEquals(TIME_ON, timerMax.get());
+		assertEquals("start", timerBtnLabel.get());
 	},
 	
 	"test clickBtn 'start' shoud change status (stop to work)": function() {
-		this.timer.setStatus("stop");
-		this.timer.clickBtn();
+		timerStatus.set("stop");
+		clickBtn();
 		
-		assertEquals("work", this.timer.getStatus());
-		assertEquals("interrupt", this.timer.getBtnLabel());
+		assertEquals("work", timerStatus.get());
+		assertEquals("interrupt", timerBtnLabel.get());
 		// タイマーが開始していることは確認してない
 	},
 	
 	"test timer=0 shoud change status (WORK to STOP)": function(queue) {
-		this.timer.setMode("off");
-		this.timer.setStatus("stop");
-		this.timer.setTime(1.0);
-		this.timer.clickBtn();
+		timerMode.set("off");
+		timerStatus.set("stop");
+		timerMax.set(1.0);
+		clickBtn();
 		
-		assertEquals("work", this.timer.getStatus());
-		assertEquals(1.0, this.timer.getTime());
+		assertEquals("work", timerStatus.get());
+		assertEquals(1.0, timerMax.get());
 		queue.call(function(callbacks) {
 			setTimeout(callbacks.add(function() {
-				assertEquals("on", this.timer.getMode());
-				assertEquals("stop", this.timer.getStatus());
-				assertEquals("start", this.timer.getBtnLabel());
+				assertEquals("on", timerMode.get());
+				assertEquals("stop", timerStatus.get());
+				assertEquals("start", timerBtnLabel.get());
 				// ウィンドウがアクティブになっているテスト？
-			}), 1100); // タイムラグのせいかテストエラー出る場合あり。ブラウザ開きっぱなしだと重くなる？
+			}), 2000); // タイムラグのせいかテストエラー出る場合あり。ブラウザ開きっぱなしだと重くなる？
 		});
 	},
 
 	//ポップアップのstartボタンを押すとoffモードでタイマースタート
 	"test timer=0 and clickBtn should change mode (ON to OFF)": function() {
-		this.timer.setMode("on");
-		this.timer.setStatus("stop");
-		this.timer.setTime(0.0);
-		this.timer.clickBtn();
+		timerMode.set("on");
+		timerStatus.set("stop");
+		timerMax.set(0.0);
+		clickBtn();
 		
-		assertEquals("off", this.timer.getMode());
-		assertEquals("work", this.timer.getStatus());
-		assertEquals("interrupt", this.timer.getBtnLabel());
+		assertEquals("off", timerMode.get());
+		assertEquals("work", timerStatus.get());
+		assertEquals("interrupt", timerBtnLabel.get());
 	},
 	
 	"test clickBtn 'interrupt' shoud change status (work to interrupt)": function() {
-		this.timer.setStatus("work");
-		this.timer.clickBtn();
+		timerStatus.set("work");
+		clickBtn();
 		
-		assertEquals("interrupt", this.timer.getStatus());
-		assertEquals("restart", this.timer.getBtnLabel());
+		assertEquals("interrupt", timerStatus.get());
+		assertEquals("restart", timerBtnLabel.get());
 		// タイマーが停止していることは確認してない
 	},
 	
-	"test clickBtn 'reset' shoud change status (interrupt to stop)": function() {
-		this.timer.setMode("on");
-		this.timer.setTime(TIME_OFF);
-		this.timer.setStatus("interrupt");
-		this.timer.clickBtn();
+	"test clickBtn 'reset' shoud change status (interrupt to work), when on mode": function() {
+		timerMode.set("on");
+		timerMax.set(TIME_ON);
+		timerStatus.set("interrupt");
+		clickBtn();
 		
-		assertEquals("on", this.timer.getMode());
-		assertEquals("work", this.timer.getStatus());
-		assertEquals(TIME_ON, this.timer.getTime());
-		assertEquals("interrupt", this.timer.getBtnLabel());
+		assertEquals("on", timerMode.get());
+		assertEquals("work", timerStatus.get());
+		assertEquals(TIME_ON, timerMax.get());
+		assertEquals("interrupt", timerBtnLabel.get());
 		// タイマーが停止していることは確認してない
 	},
 	
-	"test clickBtn 'reset' shoud change status (interrupt to stop)": function() {
-		this.timer.setMode("off");
-		this.timer.setTime(TIME_OFF);
-		this.timer.setStatus("interrupt");
-		this.timer.clickBtn();
+	"test clickBtn 'reset' shoud change status (interrupt to work), when off mode": function() {
+		timerMode.set("off");
+		timerMax.set(TIME_OFF);
+		timerStatus.set("interrupt");
+		clickBtn();
 		
-		assertEquals("off", this.timer.getMode());
-		assertEquals("work", this.timer.getStatus());
-		assertEquals(TIME_OFF, this.timer.getTime());
-		assertEquals("interrupt", this.timer.getBtnLabel());
+		assertEquals("off", timerMode.get());
+		assertEquals("work", timerStatus.get());
+		assertEquals(TIME_OFF, timerMax.get());
+		assertEquals("interrupt", timerBtnLabel.get());
 		// タイマーが停止していることは確認してない
 	},
 });
